caa054336ea272a7dfdae137b191af62fd385492
Ryan Cramer <ryan@rc-d.net>
2014-08-05 13:07:03 -0400

    Update for ProcessWire 2.5 dependencies support

---
+++ FieldtypeConcat.info.php
@@ -0,0 +1,8 @@
+<?php
+$info = array(
+	'title' => 'Concat',
+	'version' => 105,
+	'summary' => 'Concatenates the values from one or more other fields at runtime. For example, you might create a full_name field that combines the values of first_name and last_name.',
+	'author' => 'Ryan Cramer',
+	'requires' => array('FieldtypeText')
+	);
