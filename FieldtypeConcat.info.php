c0683af03d76821d6c9eaf511ff258cd6b373bdc
Ryan Cramer <ryan@rc-d.net>
2015-04-17 07:12:42 -0400

    Update to support the same field w/subfields being used more than once.

--- FieldtypeConcat.info.php
+++ FieldtypeConcat.info.php
@@ -1,7 +1,7 @@
 <?php
 $info = array(
 	'title' => 'Concat',
-	'version' => 105,
+	'version' => 106,
 	'summary' => 'Concatenates the values from one or more other fields at runtime. For example, you might create a full_name field that combines the values of first_name and last_name.',
 	'author' => 'Ryan Cramer',
 	'requires' => array('FieldtypeText')
